a2c51c7de2725e32d12e460aa64eeba1efea7662
Ronaldo <ronaldcollo172@gmail.com>
2024-11-21 20:22:29 -0500

    Crud campos y clientes

---
+++ Pages/Clientes/Clientes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportField.Pages.Clientes
+{
+    public class ClientesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
