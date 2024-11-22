5e28ad053f07d57d2d264ff63e39ef2767718879
Ronaldo <ronaldcollo172@gmail.com>
2024-11-21 20:30:57 -0500

    Creando los cruds para mis modelos

---
+++ Pages/Reservas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SportField.Pages.Reservas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
